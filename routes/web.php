bf9cfc62c83c682bc648094fdd3034b6d10fbc1e
Nayla Nabila Syahel <naylanab24@mhs.usk.ac.id>
2026-05-30 17:46:11 +0700

    feat: add dashboard owner

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OwnerDashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/owner/dashboard', [OwnerDashboardController::class, 'index'])->name('owner.dashboard');
