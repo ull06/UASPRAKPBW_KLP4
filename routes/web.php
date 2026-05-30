69c09984f7a1bea3021db850ca33e8cb9400b11b
Rahmatul Uliya <uliya24mhs.usk.ac.id>
2026-05-30 22:55:27 +0700

    fitur: beresin konflik awal breeze dan rute

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KosController;
 
+// Halaman Utama
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Route Dashboard Umum Bawaan Breeze
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+//Kelola Owner
 Route::middleware('auth')->prefix('owner')->name('owner.')->group(function () {
     Route::get('/dashboard',          [KosController::class, 'index'])->name('dashboard');
     Route::get('/kos/create',         [KosController::class, 'create'])->name('kos.create');
@@ -18,3 +26,12 @@ Route::middleware('auth')->prefix('owner')->name('owner.')->group(function () {
     Route::delete('/photo/{photo}',   [KosController::class, 'deletePhoto'])->name('photo.delete');
     Route::get('/kos/{kos}/reviews',  [KosController::class, 'reviews'])->name('kos.reviews');
 });
+
+// Kelola Profil user
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
